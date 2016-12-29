877d4c1786b09af1aa03f51042144a7f7c9dd7cc
Lekontsev <uthertg@gmail.com>
2016-12-29 18:24:13 +0500

    next_commit

--- app/assets/javascripts/modules/init.js
+++ app/assets/javascripts/modules/init.js
@@ -16,14 +16,22 @@ app.modules.init = ((self) => {
     $('.js-title-example').html(titleTemplate({title: 'Javascript tutorial part 2'}));
   }
 
+  function _getProducts() {
+    _api({url: '/api/products'}).then((response) => { app.config.products = response; });
+  }
+
+  function _getTraits() {
+    _api({url: '/api/traits'}).then((response) => { app.config.traits = response; });
+  }
+
   function _init() {
+    _getProducts();
+    _getTraits();
     _handlebarsExampleOfUsage(); // Метод, показываеющий как работать с handlebars-loader
   }
 
   self.ready = () => {
     _init();
-    _api({url: '/api/products/1', data: {traits: [1,2,3]}, method: 'PUT'}).then((response) => { console.log(response); });
-
   };
 
   return self;
--- webpack.config.js
+++ webpack.config.js
@@ -35,31 +35,145 @@ module.exports = {
           item = _productsData.data.find(item => item.id === data.id);
 
         item = Object.assign(item, data);
-        console.log(item);
+
+        if (item.values.length === data.values.length)
+
         _saveToFile('products');
+        return item;
+      }
+
+      function _toggleTraitAssignment(itemId, traitId, method) {
+        let
+          result, itemIndex, isItemIdPresent,
+          trait = _traitsData.data.find(trait => trait.id === traitId);
+
+        isItemIdPresent = !!trait['allowed_products'].filter((id, index) => {
+          let expression = id === itemId;
+
+          if (expression) {
+            itemIndex = index;
+          }
+
+          return expression;
+        }).length;
+
+        switch (method) {
+          case 'post':
+            if (isItemIdPresent) {
+              result = {error: 'Product with with id = ' + itemId + ' is already assigned, you should not make unnecessary requests'};
+            } else {
+              trait['allowed_products'].push(itemId);
+              result = trait;
+            }
+            break;
+          case 'delete':
+            if (isItemIdPresent) {
+              trait['allowed_products'].splice(itemIndex, 1);
+              result = trait;
+            } else {
+              result = {error: 'There is no product with id = ' + itemId + ' to delete, you should not make unnecessary requests'};
+            }
+            break;
+        }
+
+        _saveToFile('traits');
+        return result;
       }
 
       function _saveToFile(file) {
         switch (file) {
           case 'products':
-            fs.writeFile(DB_ROUTE + '/products.json', _productsData, 'utf-8');
+            fs.writeFile(DB_ROUTE + '/products.json', JSON.stringify(_productsData), 'utf-8');
             break;
           case 'traits':
-
+            fs.writeFile(DB_ROUTE + '/traits.json', JSON.stringify(_traitsData), 'utf-8');
             break;
         }
       }
 
+      function _filterProductsData(query) {
+        let filteredData = Object.assign({}, _productsData);
+
+        filteredData.data = filteredData.data.filter((item) => {
+          let
+            key, match;
+
+          for (key in query) {
+            match = !!item.traits.find((trait) => {
+              return (trait.slug === key) &&
+                      !!trait.values.find(value => {
+                        return query[key].includes(value.id.toString());
+                      });
+            });
+
+            if (!match) { return false; }
+          }
+
+          return match;
+        });
+
+        return filteredData;
+      }
+
+      function _updateTraits(traits) {
+        traits.forEach((trait) => {
+          if (trait.id === 'undefined') {
+            trait.id = _generateId();
+          }
+
+          trait.values.forEach((value) => {
+            if (value.id === 'undefined') {
+              value.id = _generateId();
+            }
+          });
+        });
+
+        _traitsData.data = traits;
+
+        return _traitsData;
+      }
+
+      function _generateId() {
+        return Math.floor((Math.random() * 1000000) + 1);
+      }
+
       app.use(BODY_PARSER.json());
       app.use(BODY_PARSER.urlencoded({extended: true}));
+      app.disable('etag');
 
       app
-        .get('/api/products', (req, res) => {
-          res.json(productsData);
+        .get('/', (req, res) => {
+          res.sendFile(__dirname + '/index.html');
         })
-        .put('/api/products/:id', (req, res) => {
-          _updateItem(req.params.id, req.body);
-          res.send('success');
+        .get('/admin/traits', (req, res) => {
+          res.sendFile(__dirname + '/traits.html');
+        })
+        .get('/api/products', (req, res) => {
+          if (Object.keys(req.query).length) {
+            let filteredData = _filterProductsData(req.query);
+            res.json(filteredData);
+          } else {
+            res.json(_productsData);
+          }
+        })
+        .put('/api/products/:product_id', (req, res) => {
+          let result = _updateItem(req.params.id, req.body);
+          res.status(result.error ? 422 : 200).json(result);
+        })
+        .get('/api/traits', (req, res) => {
+          res.json(_traitsData);
+        })
+        .post('/api/traits', (req, res) => {
+          let result = _updateTraits(req.body);
+          res.status(result.error ? 422 : 200).json(result);
+        })
+        .delete('/api/traits/:trait_id/products/:product_id', (req, res) => {
+          let result = _toggleTraitAssignment(parseInt(req.params['product_id']), parseInt(req.params['trait_id']), 'delete');
+          res.status(result.error ? 422 : 200).json(result);
+        })
+        .post('/api/traits/:trait_id/products/:product_id', (req, res) => {
+          let result = _toggleTraitAssignment(parseInt(req.params['product_id']), parseInt(req.params['trait_id']), 'post');
+          res.status(result.error ? 422 : 200).json(result);
         });
     }
   },
